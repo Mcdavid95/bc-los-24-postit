c965bcb46010dd06b2959b54db7f766aaa9f5ba6
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-07-07 11:49:29 +0100

    add basic route features

--- server.js
+++ server.js
@@ -10,6 +10,35 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
-  res.json()
-})
+app.get('/', (req, res) => {
+  res.redirect('/api/user/signup');
+});
+
+app.post('/api/user/signin', (req, res) => res.status(200).json({
+  message: 'welcome to the login page',
+}));
+
+app.post('/api/group/', (req, res) => {
+  res.json('I am a user');
+});
+
+app.post('/api/group/:id/user', (req, res) => {
+  res.json('I add a group user');
+});
+
+app.post('/api/group/:id/message', (req, res) => {
+  res.json('I am a message');
+});
+
+app.post('/api/group/:id/messages', (req, res) => {
+  res.json('I messages in a group');
+});
+
+app.get('*', (req, res) => res.status(200).json({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+const port = 1337;
+const server = app.listen(process.env.PORT || port, () => (`LISTENING ON PORT ${port}...`));
+export default server;
+
