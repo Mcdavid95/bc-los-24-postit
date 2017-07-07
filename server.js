51e80d011f9df8170c721ea2b600fb6207920a0a
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-07-07 08:42:24 +0100

    feature: add entry-point to app

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('*', re)
