44cc7c468b8705a1a8c49f27b3a443b4a15dd7be
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-07-10 13:48:37 +0100

    feature: create migration file for models

---
+++ server/migrations/20170710095759-create-group.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      picture: {
+        type: Sequelize.URL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'User',
+          key: 'id',
+          as: 'userId',
+        },
+      },
+
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Groups');
+  }
+};
