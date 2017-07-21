943248a12f56e0256abf6e7221c638ea0d03b164
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-07-21 19:19:46 +0100

    chore: add seed files to test authentication routes

---
+++ server/seeders/authSeeds.js
@@ -0,0 +1,20 @@
+export const valid = {
+  username: 'Mcdavid',
+  password: 'janike_13',
+  email: 'mcdavidemereuwa95@gmail.com',
+  phoneNumber: 8173753955
+};
+
+export const anotherValid = {
+  username: 'Melody',
+  password: 'melo_13',
+  email: 'melody@gmail.com',
+  phoneNumber: 8167188889
+};
+
+export const invalidUser = {
+  username: 'Steven',
+  password: 'step_hen',
+  email: 'steve@gmail.com',
+  phoneNumber: 8033379342
+};
