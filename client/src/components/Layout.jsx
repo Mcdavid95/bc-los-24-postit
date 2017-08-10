8987a2f18234ea5e5cfd6cfd4d4bd70751440551
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-08-10 10:47:18 +0100

    (fix): revert to previous state

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -5,14 +5,13 @@ import Header from './Header/Header.jsx';
 export default class Layout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { path: '/' };
   }
   render() {
     return (
       <div>
         <Header />
         <h1> Welcome to Postit!! Mcdavid </h1>
-        <Link onClick={this.setState({ path: 'login' })} to="/signup" className="btn red lighten-1 btn-sm">Register</Link>
+        <Link  to="/signup" className="btn red lighten-1 btn-sm">Register</Link>
         <Link to="/login">Login</Link>
       </div>
     );
