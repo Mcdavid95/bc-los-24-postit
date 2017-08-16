e644e9d84eea69de49752948ea7db6aeb5af49f1
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-08-16 06:51:24 +0100

    chore: configure webpack to be run from server

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,58 @@
+const debug = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
 
 module.exports = {
-  entry: './client/src/index.jsx',
-  output: {
-    path: path.resolve('client/js'),
-    filename: 'index.bundle.js'
-  },
-  watch: true,
+  devtool: debug ? 'inline-sourcemap' : true,
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true',
+    path.resolve(__dirname, 'client/index.jsx')
+  ],
+
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+      { test: /\.(css|scss)$/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.(js|jsx)$/, include: path.join(__dirname, 'client'), loader: 'babel-loader', query: { presets: ['es2015', 'react'] } },
+      // { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
       },
-      {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        loader: ['style-loader', 'css-loader', 'sass-loader']
-      }
     ]
   },
 
+  watch: true,
+
   devServer: {
     historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'client')
   },
 
-  plugins: [HtmlWebpackPluginConfig]
+  output: {
+    path: `${__dirname}/client/dist/`,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Hammer: 'hammerjs/hammer'
+    }),
+    // new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+
+  resolve: {
+    // you can now require("file") instead of require("file.coffee")
+    extensions: ['.js', '.json', '.jsx']
+  },
+
+  node: {
+    net: 'empty',
+    dns: 'empty'
+  }
+
 };
