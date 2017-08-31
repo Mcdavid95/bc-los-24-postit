960be1188e529323d3ffdc7993696f35d6543241
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-08-31 04:59:09 +0100

    feat(scss): implement feedback
    - change from css to scss
    - add paths for login logout and groups

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt from 'jsonwebtoken';
+import Group from '../client/src/containers/GroupForm';
 
 import Board from './src/pages/MessageBoard/Board';
 import Layout from './src/components/Layout';
@@ -12,10 +13,12 @@ import configureStore from './src/store';
 import history from './src/utils/History';
 import setAuthToken from './src/utils/setAuthToken';
 import setCurrentUser from './src/actions/currentUserActions';
+import loadGroups from './src/actions/loadUserGroupListActions';
 import requireAuth from './src/utils/Authenticate';
 
 import './public/materialize.min.css';
-import './public/auth.css';
+// import './public/auth.css';
+import './public/auth.scss';
 import './js/jquery-3.2.1.min';
 import './js/materialize.min';
 import './js/post';
@@ -27,6 +30,7 @@ if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
   store.dispatch(setCurrentUser(jwt.decode(localStorage.jwtToken)));
 }
+store.dispatch(loadGroups());
 
 ReactDom.render(
   <Provider store={store}>
@@ -34,9 +38,10 @@ ReactDom.render(
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/layout" component={Layout} />
-        <Route exact path="/api/user/register" name="signup" component={Signup} />
-        <Route exact path="/api/user/login" name="login" component={Login} />
+        <Route exact path="/register" name="signup" component={Signup} />
+        <Route exact path="/login" name="login" component={Login} />
         <Route exact path="/dashboard" name="board" component={requireAuth(Board)} />
+        <Route exact path="/group" name="group" component={requireAuth(Group)} />
       </Switch>
     </Router>
   </Provider>
