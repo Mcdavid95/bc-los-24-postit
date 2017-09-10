29b37a486b7e396babaef5e89e5e486a17b212f0
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-09-10 13:02:27 +0100

    feat(get-all-users): fetch all user data from api and update redux-store

---
+++ client/src/actions/getAllUsersActions.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import * as types from '../../constant';
+
+const getAllUsersSuccess = users => ({ type: types.GET_USERS_SUCCESS, users });
+
+const getAllUsersFailed = users => ({ type: types.GET_USERS_FAILED, users });
+
+const getAllUsers = () => dispatch =>
+  axios.get('/api/users')
+    .then((response) => {
+      dispatch(getAllUsersSuccess(response.data));
+    })
+    .catch((response) => {
+      dispatch(getAllUsersFailed(response.data));
+    });
+
+export default getAllUsers;
