6a26a11b1ff5ba63817c35ca69bd453ac62bf070
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-10-06 09:06:10 +0100

    chore(test): write front-end tests

---
+++ client/_tests_/containers/SignupForm.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+// import configureStore from 'redux-mock-store';
+import SignupForm from '../../src/containers/SignupForm';
+
+describe('Signup form component test', () => {
+  const props = {
+    userSignupRequest: jest.fn(() => Promise.resolve())
+  };
+  it('should render without crashing', () => {
+    const component = shallow(<SignupForm />);
+    expect(component.node.type).toBe('div');
+  });
+  it('should contain the method onChange', () => {
+    const component = shallow(<SignupForm {...props} />);
+    const onChangeSpy = jest.spyOn(component.instance(), 'onChange');
+    component.instance().onChange({
+      target: {
+        name: 'username', value: 'mcdavid'
+      }
+    });
+    expect(onChangeSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should contain the method onSubmit', () => {
+    const e = {
+      preventDefault: jest.fn()
+    };
+    const component = shallow(<SignupForm {...props} />);
+    const onSubmitSpy = jest.spyOn(component.instance(), 'onSubmit');
+    component.instance().onSubmit(e);
+    expect(onSubmitSpy).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ client/_tests_/reducer_tests/signupReducer.test.js
@@ -0,0 +1,67 @@
+import signup from '../../src/reducers/signupReducer';
+import reducers from '../../src/reducers';
+import * as types from '../../constant';
+
+test('reducers', () => {
+  const state = reducers({
+    signup: { username: '', email: '', phoneNumber: 234, loggedIn: false, password: '' },
+    login: [null],
+    setAuthToken: { isAuthenticated: true, user: { id: 1, name: 'mcdavid', email: 'mcdavidemereuwa95@gmail.com', iat: 1505944828, exp: 1506031228 } },
+    addflashMessages: [],
+    group: { GroupName: '', description: '' },
+    userGroupList: [[{ groupName: 'jonny', groupId: 1, description: 'for yemi alade' }]],
+    groupMessages: [],
+    postMessage: { message: '', priority: 'normal' },
+    addUser: { username: '' },
+    allUsers: [],
+    forgotPassword: { email: '' },
+    resetPassword: { newPassword: '', confirmPassword: '' },
+    search: { username: '' } },
+  { type: 'GET_MESSAGES_SUCCESS',
+    messages: [{ id: 1, message: 'hi', userId: 1, groupId: 1, username: 'mcdavid', priority: 'normal' }] });
+  expect(state).toEqual({ signup:
+    { username: '', email: '', phoneNumber: 234, loggedIn: false, password: '' },
+  login: [null],
+  setAuthToken: { isAuthenticated: true, user: { id: 1, name: 'mcdavid', email: 'mcdavidemereuwa95@gmail.com', iat: 1505944828, exp: 1506031228 } },
+  addflashMessages: [],
+  group: { GroupName: '', description: '' },
+  userGroupList: [[{ groupName: 'jonny', groupId: 1, description: 'for yemi alade' }]],
+  groupMessages: [[{ id: 1, message: 'hi', userId: 1, groupId: 1, username: 'mcdavid', priority: 'normal' }]],
+  postMessage: { message: '', priority: 'normal' },
+  addUser: { username: '' },
+  allUsers: [],
+  forgotPassword: { email: '' },
+  resetPassword: { newPassword: '', confirmPassword: '' },
+  search: { username: '' } });
+});
+
+
+describe('Signup Reducer', () => {
+  it('SIGNUP_USER success', () => {
+    const state = {
+      username: '',
+      email: '',
+      phoneNumber: 234,
+      loggedIn: false,
+      password: ''
+    };
+    const action = {
+      type: types.SIGNUP_USER,
+      username: 'Mcdavid',
+      email: 'mcdavidemereuwa@gmail.com',
+      phoneNumber: 2333405585,
+      loggedIn: false,
+      password: 'jhiuiui'
+    };
+    const results = signup(state, action);
+    expect(results)
+      .toEqual([{
+        type: 'SIGN_UP_USER_SUCCESS',
+        username: 'Mcdavid',
+        email: 'mcdavidemereuwa@gmail.com',
+        phoneNumber: 2333405585,
+        loggedIn: false,
+        password: 'jhiuiui'
+      }]);
+  });
+});
