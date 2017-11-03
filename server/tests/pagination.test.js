669942e8949385f8fcfc737f954b1b33c491b9d5
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-11-03 11:40:04 +0100

    chore(pagination): fix pagination test

--- server/tests/pagination.test.js
+++ server/tests/pagination.test.js
@@ -4,13 +4,23 @@ import paginate from '../controllers/pagination';
 const expect = chai.expect;
 
 describe('Pagination method', () => {
-  it('should return an object with the page, pageCount and pageSize', () => {
+  it('should return an object with the page, pageCount and pageSize when (count - limit) !> limit', () => {
     const result = {
       page: 2,
       pageCount: 2,
       pageSize: 5,
       count: 10
     };
-    expect(paginate(10, 5, 5).should.to.be(result));
+    expect(paginate(10, 5, 5).should.deep.equals(result));
+  });
+
+  it('should return an object with the page, pageCount and pageSize when (count - limit) > limit', () => {
+    const result = {
+      page: 2,
+      pageCount: 3,
+      pageSize: 5,
+      count: 15
+    };
+    expect(paginate(15, 5, 5).should.deep.equals(result));
   });
 });
